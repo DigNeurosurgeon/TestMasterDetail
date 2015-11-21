eb452545ebac05abe688431ecf7c5c9ac0336b6b
Pieter Kubben <pieter@kubben.nl>
2015-11-21 23:04:15 +0100

    Cleaned template version

--- testMasterDetail/DetailViewController.swift
+++ testMasterDetail/DetailViewController.swift
@@ -29,17 +29,6 @@ class DetailViewController: UIViewController {
         }
     }
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-        self.configureView()
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
 
 }
 
